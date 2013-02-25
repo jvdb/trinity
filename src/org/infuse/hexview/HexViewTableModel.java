d14de2ab98c4c88f5653f5e3c1f9fd1ec8b10239
jvdb <jeroen@infuse.org>
2013-02-25 20:37:32 +0100

    Fixed bug where the first column showed the row number instead of the row offset. Changed fixed width value into a constant.

--- src/org/infuse/hexview/HexViewTableModel.java
+++ src/org/infuse/hexview/HexViewTableModel.java
@@ -9,6 +9,8 @@ import javax.swing.table.AbstractTableModel;
 @SuppressWarnings("serial")
 public class HexViewTableModel extends AbstractTableModel {
   
+  public final int WIDTH = 16;
+  
   private byte[] _data;
   private final String[] _names = new String[] { "Offset", "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "0A", "0B", "0C", "0D", "0E", "0F", "Ascii" };
   
@@ -29,7 +31,7 @@ public class HexViewTableModel extends AbstractTableModel {
   
   @Override
   public int getRowCount() {
-    return (_data.length / 16) + ((_data.length % 16) > 1 ? 1 : 0);
+    return (_data.length / WIDTH) + ((_data.length % WIDTH) > 1 ? 1 : 0);
   }
   
   @Override
@@ -54,10 +56,10 @@ public class HexViewTableModel extends AbstractTableModel {
   @Override
   public Object getValueAt(int rowIndex, int columnIndex) {
     if (columnIndex == 0) {
-      return String.format("%08X", rowIndex);
+      return String.format("%08X", rowIndex * WIDTH);
     } else if (columnIndex == 17) {
-      int offset = rowIndex * 16;
-      int count = _data.length - offset >= 16 ? 16 : _data.length - offset;
+      int offset = rowIndex * WIDTH;
+      int count = _data.length - offset >= WIDTH ? WIDTH : _data.length - offset;
       String ret = "";
       for (int i = 0; i < count; i++) {
         int value = _data[offset+i] & 0xFF;
@@ -69,11 +71,11 @@ public class HexViewTableModel extends AbstractTableModel {
       }
       return ret;
     } else {
-      int offset = (rowIndex*16)+(columnIndex-1);
+      int offset = (rowIndex*WIDTH)+(columnIndex-1);
       if (offset >= _data.length) {
         return "";
       } else {
-        return String.format("%02X", _data[(rowIndex*16)+(columnIndex-1)] & 0xFF);
+        return String.format("%02X", _data[(rowIndex*WIDTH)+(columnIndex-1)] & 0xFF);
       }
     }
   }
