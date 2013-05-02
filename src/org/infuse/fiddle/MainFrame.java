08f9972231b39b40a58795cafb412bb4d78636cf
jvdb <jeroen@infuse.org>
2013-05-02 17:44:52 +0200

    Hex view selection handling should also occur when regaining focus, to prevent a selection from not being handled when the focus is regained on the same cell.

--- src/org/infuse/fiddle/MainFrame.java
+++ src/org/infuse/fiddle/MainFrame.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
@@ -72,20 +74,20 @@ public class MainFrame extends JFrame {
             @Override
             public void valueChanged(ListSelectionEvent e) {
                 if (!e.getValueIsAdjusting()) {
-                    if (_hexView.getSelectedColumn() > 0 && _hexView.getSelectedColumn() <= HexViewTableModel.WIDTH) {
-                        int offset = (_hexView.getSelectedRow() * HexViewTableModel.WIDTH) + (_hexView.getSelectedColumn() - 1);
-                        StructureMatch match = _current.getDataMatch(offset);
-                        setHighlightFromHexView(match);
-                    } else {
-                        clearSelections();
-                        clearHighlights();
-                    }
-                    _hexView.repaint();
+                    handleHexSelection();
                 }
-            } 
+            }
         };
         _hexView.getSelectionModel().addListSelectionListener(sl);
         _hexView.getColumnModel().getSelectionModel().addListSelectionListener(sl);
+        _hexView.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                handleHexSelection();
+            }
+            @Override
+            public void focusLost(FocusEvent e) { }
+        });
         
         // Code view
         _codeView = new CodeEditorPane();
@@ -190,6 +192,18 @@ public class MainFrame extends JFrame {
     private HashMap<String, Color> getDerricColors() {
         HashMap<String, Color> syntax = new HashMap<String, Color>();
         return syntax;
+    }
+    
+    private void handleHexSelection() {
+        if (_hexView.getSelectedColumn() > 0 && _hexView.getSelectedColumn() <= HexViewTableModel.WIDTH) {
+            int offset = (_hexView.getSelectedRow() * HexViewTableModel.WIDTH) + (_hexView.getSelectedColumn() - 1);
+            StructureMatch match = _current.getDataMatch(offset);
+            setHighlightFromHexView(match);
+        } else {
+            clearSelections();
+            clearHighlights();
+        }
+        _hexView.repaint();
     }
     
     private void setHighlightFromHexView(StructureMatch match) {
